15c3e288db99451c1bce5ddfecd4cd4408f0a8d4
Isham Faizal <ishamf@users.noreply.github.com>
2023-06-15 01:49:15 +0800

    chore: clean up some debug code

--- src/routes/e/[slug]/+page.svelte
+++ src/routes/e/[slug]/+page.svelte
@@ -2,19 +2,11 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-
-	$: console.log('Metadata: ', data.metadata);
 </script>
 
-<div class="prose prose-slate dark:prose-invert font-sans mx-auto mt-4">
+<div class="prose prose-slate dark:prose-invert font-sans mx-auto mt-4 px-2">
 	<svelte:component this={data.component} />
 </div>
-
-<div> asd asd </div>
-
-<pre>
-	asd   asd
-</pre>
 
 <style>
 </style>
