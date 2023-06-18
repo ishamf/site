192e4e6bf7c7559a8b7463510770a009ed65fb2e
Isham Faizal <ishamf@users.noreply.github.com>
2023-06-18 19:59:47 +0800

    feat: change to static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { mdsvex } from 'mdsvex';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import yaml from 'js-yaml';
@@ -31,10 +31,9 @@ const config = {
 	extensions: ['.svelte', '.mdx'],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			pages: '.svelte-kit/cloudflare',
+		}),
 	},
 };
 
