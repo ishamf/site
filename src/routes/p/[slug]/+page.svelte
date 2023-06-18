278add919016f3ab65f8b5678561759c5d369e5f
Isham Faizal <ishamf@users.noreply.github.com>
2023-06-18 19:56:43 +0800

    chore: fix space

--- src/routes/p/[slug]/+page.svelte
+++ src/routes/p/[slug]/+page.svelte
@@ -8,7 +8,7 @@
 
 <div class="max-w-xl mx-auto px-2 mt-4">
 	<PostHeading {title} {link} {created} />
-	<div class="prose prose-slate dark:prose-invert font-sans mx-auto mt-4  max-w-none">
+	<div class="prose prose-slate dark:prose-invert font-sans mx-auto mt-4 max-w-none">
 		<svelte:component this={data.component} />
 	</div>
 </div>
