ed5584aba7cc97e15b95448193a8b91075e21af2
Isham Faizal <ishamf@users.noreply.github.com>
2023-06-21 01:06:50 +0800

    chore: fix formatting

--- src/lib/posts/2023-06-20-url-tools/components/Button.svelte
+++ src/lib/posts/2023-06-20-url-tools/components/Button.svelte
@@ -11,10 +11,14 @@
 bg-slate-100 enabled:hover:bg-slate-200 disabled:bg-slate-200
 dark:bg-slate-700 dark:enabled:hover:bg-slate-600 dark:disabled:bg-slate-500"
 >
-	<svg viewBox="0 0 24 24" role="img" class="m-2
+	<svg
+		viewBox="0 0 24 24"
+		role="img"
+		class="m-2
     fill-slate-400 group-disabled:fill-slate-300
-    dark:fill-slate-300 dark:group-disabled:fill-slate-400">
-        <title>{title}</title>
+    dark:fill-slate-300 dark:group-disabled:fill-slate-400"
+	>
+		<title>{title}</title>
 		<path d={icon} />
 	</svg>
 </button>
