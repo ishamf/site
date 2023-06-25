02073ec8159b63ddcfb460cd0ff53e7880dc2c4a
Isham Faizal <ishamf@users.noreply.github.com>
2023-06-25 16:50:10 +0800

    feat: reduce component sizes

--- src/lib/components/PostHeading.svelte
+++ src/lib/components/PostHeading.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { DateTime } from 'luxon';
-
 	export let title: string;
 	export let link: string;
 	export let created: Date;
@@ -10,6 +8,6 @@
 	<h2 class="text-3xl font-sans text-slate-900 dark:text-slate-200">{title}</h2>
 </a>
 
-<p class="text-sm text-slate-700 dark:text-slate-300 mt-2">
-	{DateTime.fromJSDate(created).toFormat('dd LLL yyyy')}
-</p>
+<date class="block text-sm text-slate-700 dark:text-slate-300 mt-2" datetime={created.toISOString()}>
+	{created.toLocaleDateString(undefined, { day: 'numeric', month: 'short', year: 'numeric' })}
+</date>
