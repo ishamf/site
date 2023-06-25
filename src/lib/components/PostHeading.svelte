2db2e98c41270512a1f2c23f88d5e8c9686c8637
Isham Faizal <ishamf@users.noreply.github.com>
2023-06-25 16:58:30 +0800

    chore: fix lint

--- src/lib/components/PostHeading.svelte
+++ src/lib/components/PostHeading.svelte
@@ -8,6 +8,9 @@
 	<h2 class="text-3xl font-sans text-slate-900 dark:text-slate-200">{title}</h2>
 </a>
 
-<date class="block text-sm text-slate-700 dark:text-slate-300 mt-2" datetime={created.toISOString()}>
+<date
+	class="block text-sm text-slate-700 dark:text-slate-300 mt-2"
+	datetime={created.toISOString()}
+>
 	{created.toLocaleDateString(undefined, { day: 'numeric', month: 'short', year: 'numeric' })}
 </date>
